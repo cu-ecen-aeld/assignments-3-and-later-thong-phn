49bcbafaa42f3d3131d679912559035588d0ef9c
Thong Phan <mrthonglion@gmail.com>
2022-09-02 03:43:46 -0700

    Upload

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -41,9 +41,10 @@ else
 	exit 1
 fi
 
-echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+# Remove the make step
+# echo "Removing the old writer utility and compiling as a native application"
+# make clean
+# make
 
 for i in $( seq 1 $NUMFILES)
 do

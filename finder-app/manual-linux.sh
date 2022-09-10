739400c2d00d70949d94adff84aa495fa2cebc1f
thong-phn <mrthonglion@gmail.com>
2022-09-10 12:06:47 +0700

    A3-P2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -2,14 +2,14 @@ set -e
 set -u
 
 OUTDIR=/tmp/aeld
-#KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+#KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
 KERNEL_VERSION=v5.1.10
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-PROJECT_PATH=/var/tmp/
+PROJECT_PATH=/tmp/
 
 if [ $# -lt 1 ]
 then
@@ -82,8 +82,8 @@ mkdir -p var/log
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-#git clone git://busybox.net/busybox.git
-git clone https://git.busybox.net/busybox
+git clone git://busybox.net/busybox.git
+#git clone https://git.busybox.net/busybox
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
